399bc2544061f6752215482af4de48ee0892ebe5
Anumod-c <anumodc2001@gmail.com>
2024-11-12 19:10:54 +0530

    feat(logging): add Morgan for HTTP request logging

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import http from 'http'
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import  morgan from 'morgan';
+import fs from 'fs'
+import  path from 'path'
 import { initializeSocket } from './socket/socketServer';
 import config from './config';
 import { userRouter } from './modules/user/routes';
@@ -11,22 +14,27 @@ import {courseRouter} from './modules/course/routes'
 import {  orderRouter } from './modules/orders/routes';
 const app = express();
 
-
-
-
 // CORS Configuration
 const corsOptions = {
     origin:'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    credentials: true,// Allows cookies and credentials to be included
-   
-  };
-  
-  app.use(cors(corsOptions));
+    credentials: true,// Allows cookies and credentials to be included   
+};
+
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 
 
+const logDir = path.join(__dirname, 'logs');
+if (!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir);
+}
+
+const logStream = fs.createWriteStream(path.join(logDir, 'access.log'), { flags: 'a' });
+
+app.use(morgan('combined'));  // Logs to the terminal
+app.use(morgan('combined', { stream: logStream })); // Logs to the access.log file
 app.use('/admin', adminRouter);
 app.use('/tutor',tutorRouter);
 app.use('/course',courseRouter);
